0ff88ac1a724c4fb3e5b8e4403a96487abe3b53a
Phil Tom <philtom@uchicago.edu>
2021-03-02 20:20:36 -0600

    removing unused dependencies

--- setup.py
+++ setup.py
@@ -7,10 +7,6 @@ setup(name='gdc-docs',
       packages=find_packages(),
       install_requires=[
         "mkdocs>=0.15.1,<1",
-        "mkdocs-bootstrap>=0.1.1,<1",
-        "mkdocs-bootswatch>=0.4,<1",
         "BSCodeTabs>=1",
-        "Markdown<3",
-        "MarkupSafe>=1.0",
       ]
 )
